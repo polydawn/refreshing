ae36849653c46305338837748fd0d2225819dcc6
Eric Myhre <hash@exultant.us>
2017-10-29 01:14:15 +0200

    add second step to go demo which runs the binary (leaving behind the toolchain, which is trivial, as it should be).
    Signed-off-by: Eric Myhre <hash@exultant.us>

--- fling.d/cmd.demo-go.sh
+++ fling.d/cmd.demo-go.sh
@@ -34,5 +34,42 @@ EOF
 echo "$frm" | jq .formula
 
 >&2 echo Running formula the First...
-rr1="$(repeatr run <(echo "$frm"))"
-echo "$rr1" | jq .
+rr="$(repeatr run <(echo "$frm"))"
+echo "$rr" | jq .
+
+# Pass on the binary in another hitch release.
+#  But first, destroy existing releases.  this is demo content.
+rm -rf hitch.db/refreshing.polydawn.net/demo/hello-go || true
+
+hitch catalog create "refreshing.polydawn.net/demo/hello-go"
+hitch release start  "refreshing.polydawn.net/demo/hello-go" "vtmp"
+hitch release add-item "linux-amd64" "$(echo "$rr" | jq -r '.results["/task/target"]')"
+hitch release commit
+
+# Let's make another formula that runs the product, whee!
+>&2 echo Templating formula the Second...
+frm="$(cat <<EOF
+	{
+		"formula": {
+			"inputs": {
+				"/":         "$(hitch show "refreshing.polydawn.net/bases/busybash:v1:linux-amd64")",
+				"/task":     "$(hitch show "refreshing.polydawn.net/demo/hello-go:vtmp:linux-amd64")"
+			},
+			"action": {
+				"exec": ["/task/hello"]
+			}
+		},
+		"context": {
+			"fetchUrls": {
+				"/":       ["ca+file://.warehouse/", "ca+https://repeatr.s3.amazonaws.com/warehouse/"],
+				"/task":   ["ca+file://.warehouse/"]
+			}
+		}
+	}
+EOF
+)"
+echo "$frm" | jq .formula
+
+>&2 echo Running formula the Second...
+rr="$(repeatr run <(echo "$frm"))"
+echo "$rr" | jq .

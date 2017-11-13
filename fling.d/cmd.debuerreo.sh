c65593b9fb41d1c0e185c0de85c8386559247e62
Eric Myhre <hash@exultant.us>
2017-11-13 16:39:12 +0100

    we can have yaml now!
    multi-line strings really are necessary to not lose your mind after a while.
    
    We could also have just put the script in another file nearby, and used a scan or similar technique to get it into place, but that's less fun, and it's often nice to *see* the whole script in one go.
    
    Signed-off-by: Eric Myhre <hash@exultant.us>

--- fling.d/cmd.debuerreo.sh
+++ fling.d/cmd.debuerreo.sh
@@ -16,39 +16,48 @@ hitch release commit
 ##  - we do include 'ca-certificates'.  This is not great for updatability, but you can overlay it with a newer one if you want.  We figured it's better to be not-broken out of box.
 ##  - we do set a default '/etc/resolv.conf' file.  Again, the reasoning is based on practicality and wanting things to work out of box.
 frm="$(cat <<EOF
-	{
-		"formula": {
-			"inputs": {
-				"/":                  "$(hitch show "refreshing.polydawn.net/bases/mindeb:v0.1.1.1:linux-amd64")",
-				"/app/debootstrap":   "tar:2oEopZrP5ah4Aj1iJYVwx5w28QXmKE9xQW1FAK6CpHsEvke1CUqLjFZ4HnWocveNiy",
-				"/app/debuerreotype": "$(hitch show "refreshing.polydawn.net/base-factory/debuerreotype:vtmp:script")"
-			},
-			"action": {
-				"policy": "sysad",
-				"userinfo": {"uid":0},
-				"env": {
-					"DISTRIBUTION": "stretch",
-					"TIMESTAMP": "2017-01-04T03:32:24Z",
-					"DEBOOTSTRAP_DIR": "/app/debootstrap/debootstrap-1.0.87/",
-					"PATH": "/app/debuerreotype/scripts/:/app/debootstrap/debootstrap-1.0.87/:/sbin:/usr/sbin:/bin:/usr/bin"
-				},
-				"exec": ["/bin/bash", "-c", "set -euo pipefail; apt update ; apt install -y wget ; debuerreotype-init --no-merged-usr /out \$DISTRIBUTION \$TIMESTAMP ; debuerreotype-minimizing-config /out ; debuerreotype-apt-get /out update -qq ; debuerreotype-apt-get /out dist-upgrade -yqq ; debuerreotype-apt-get /out install -y --no-install-recommends inetutils-ping iproute2 wget curl ca-certificates ; debuerreotype-slimify /out ; debuerreotype-gen-sources-list /out \$DISTRIBUTION http://deb.debian.org/debian http://security.debian.org ; echo 'nameserver 8.8.8.8' > /etc/resolv.conf ; "]
-			},
-			"outputs": {
-				"/out": {"packtype": "tar", "filters": {"uid":"keep", "gid":"keep", "sticky":"keep"}}
-			}
-		},
-		"context": {
-			"fetchUrls": {
-				"/":                  ["ca+file://.warehouse/", "ca+https://repeatr.s3.amazonaws.com/warehouse/"],
-				"/app/debootstrap":   ["http://snapshot.debian.org/archive/debian/20170104T033224Z/pool/main/d/debootstrap/debootstrap_1.0.87.tar.gz"],
-				"/app/debuerreotype": ["ca+file://.warehouse/"]
-			},
-			"saveUrls": {"/out": "ca+file://.warehouse/"}
-		}
-	}
+	formula:
+		inputs:
+			"/":                  "$(hitch show "refreshing.polydawn.net/bases/mindeb:v0.1.1.1:linux-amd64")"
+			"/app/debootstrap":   "tar:2oEopZrP5ah4Aj1iJYVwx5w28QXmKE9xQW1FAK6CpHsEvke1CUqLjFZ4HnWocveNiy"
+			"/app/debuerreotype": "$(hitch show "refreshing.polydawn.net/base-factory/debuerreotype:vtmp:script")"
+		action:
+			policy: "sysad"
+			userinfo: {"uid":0, "gid":0}
+			env:
+				"DISTRIBUTION": "stretch"
+				"TIMESTAMP": "2017-01-04T03:32:24Z"
+				"DEBOOTSTRAP_DIR": "/app/debootstrap/debootstrap-1.0.87/"
+				"PATH": "/app/debuerreotype/scripts/:/app/debootstrap/debootstrap-1.0.87/:/sbin:/usr/sbin:/bin:/usr/bin"
+			exec:
+				- "/bin/bash"
+				- "-c"
+				- |
+					set -euo pipefail
+					apt update
+					apt install -y wget
+					debuerreotype-init --no-merged-usr /out \$DISTRIBUTION \$TIMESTAMP
+					debuerreotype-minimizing-config /out
+					debuerreotype-apt-get /out update -qq
+					debuerreotype-apt-get /out dist-upgrade -yqq
+					debuerreotype-apt-get /out install -y --no-install-recommends inetutils-ping iproute2 wget curl ca-certificates
+					debuerreotype-slimify /out
+					debuerreotype-gen-sources-list /out \$DISTRIBUTION http://deb.debian.org/debian http://security.debian.org
+					echo 'nameserver 8.8.8.8' > /out/etc/resolv.conf
+		outputs:
+			"/out":
+				packtype: "tar"
+				filters: {"uid":"keep", "gid":"keep", "sticky":"keep"}
+	context:
+		fetchUrls:
+			"/":                  ["ca+file://.warehouse/", "ca+https://repeatr.s3.amazonaws.com/warehouse/"]
+			"/app/debootstrap":   ["http://snapshot.debian.org/archive/debian/20170104T033224Z/pool/main/d/debootstrap/debootstrap_1.0.87.tar.gz"]
+			"/app/debuerreotype": ["ca+file://.warehouse/"]
+		saveUrls:
+			"/out": "ca+file://.warehouse/"
 EOF
 )"
+frm="$(echo "$frm" | refmt yaml=json)"
 echo "$frm" | jq .formula
 
 >&2 echo Running formula the First...
